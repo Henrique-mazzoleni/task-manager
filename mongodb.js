697e9a2d7f2bc100faebcbef5a84822360cf7de8
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2021-10-25 21:16:56 +0200

    populating the database

--- mongodb.js
+++ mongodb.js
@@ -12,9 +12,53 @@ MongoClient.connect(
 
     const db = client.db(databaseName);
 
-    db.collection("users").insertOne({
-      name: "Herique",
-      age: 37,
-    });
+    db.collection("users").insertOne(
+      {
+        name: "Herique",
+        age: 37,
+      },
+      (error, result) => {
+        if (error) return console.log("Unable to insert user");
+        console.log(result);
+      }
+    );
+
+    db.collection("users").insertMany(
+      [
+        {
+          name: "Annia",
+          age: 37,
+        },
+        {
+          name: "Gabriella",
+          age: 0,
+        },
+      ],
+      (error, result) => {
+        if (error) return console.log("Unable to insert documents");
+        console.log(result);
+      }
+    );
+
+    db.collection("tasks").insertMany(
+      [
+        {
+          description: "comprar capa bike",
+          completed: false,
+        },
+        {
+          description: "buscar passaporte Gabriella",
+          completed: false,
+        },
+        {
+          description: "cobrar passaporte Italiano",
+          completed: true,
+        },
+      ],
+      (error, result) => {
+        if (error) return console.log("Unable to insert documents");
+        console.log(result);
+      }
+    );
   }
 );
