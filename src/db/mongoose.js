5dbc71e21c686638f1395d4e7c59996952531084
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2021-10-27 20:48:21 +0200

    API endpoint for Task implemented

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,16 +1,3 @@
 const mongoose = require("mongoose");
-const validator = require("validator");
 
 mongoose.connect("mongodb://mongodb:27017/task-manage-api");
-
-const Task = mongoose.model("Task", {
-  description: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  completed: {
-    type: Boolean,
-    default: false,
-  },
-});
