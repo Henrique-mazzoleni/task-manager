494b25fc515ebb01a6fa5d0ac457d7dbf1ce4f3c
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2021-10-28 20:20:04 +0200

    added fetch users endpoint

--- src/index.js
+++ src/index.js
@@ -20,6 +20,27 @@ app.post("/users", (req, res) => {
     });
 });
 
+app.get("/users", (req, res) => {
+  User.find({})
+    .then((users) => {
+      res.send(users);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+});
+
+app.get("/users/:id", (req, res) => {
+  User.findById(req.params.id)
+    .then((user) => {
+      if (!user) return res.status(404).send();
+      res.send(user);
+    })
+    .catch((error) => {
+      res.status(500).send();
+    });
+});
+
 app.post("/tasks", (req, res) => {
   const task = new Task(req.body);
   task
