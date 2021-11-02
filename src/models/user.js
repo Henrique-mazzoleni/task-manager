0ad36ca935f675ec047c5c2607c42f90bdaffb84
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2021-11-02 20:49:08 +0100

    task cascade delete

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const bcript = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const Task = require("../models/task");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -90,6 +91,12 @@ userSchema.pre("save", async function (next) {
     user.password = await bcript.hash(user.password, 8);
   }
 
+  next();
+});
+
+userSchema.pre("remove", async function (next) {
+  const user = this;
+  await Task.deleteMany({ owner: user._id });
   next();
 });
 
