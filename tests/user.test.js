3a7730d9625ad32c9a1d6e3331623af71e82488e
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2021-11-07 14:44:48 +0100

    expanding testbase

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,5 +1,26 @@
 const request = require("supertest");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
 const app = require("../src/app");
+const User = require("../src/models/user");
+
+const userOneId = new mongoose.Types.ObjectId();
+const userOne = {
+  _id: userOneId,
+  name: "Henrique",
+  email: "henrique@gmail.com",
+  password: "123de456",
+  tokens: [
+    {
+      token: jwt.sign({ _id: userOneId }, process.env.TOKEN_PHRASE),
+    },
+  ],
+};
+
+beforeEach(async () => {
+  await User.deleteMany();
+  await new User(userOne).save();
+});
 
 test("Should sign up new user", async () => {
   await request(app)
@@ -10,4 +31,48 @@ test("Should sign up new user", async () => {
       password: "Punzeira",
     })
     .expect(201);
+});
+
+test("Should login user", async () => {
+  await request(app)
+    .post("/users/login")
+    .send({
+      email: userOne.email,
+      password: userOne.password,
+    })
+    .expect(200);
+});
+
+test("Should not login inexistent user", async () => {
+  await request(app)
+    .post("/users/login")
+    .send({
+      email: "Frambofritz@gmail.com",
+      password: "!!!what!!!",
+    })
+    .expect(400);
+});
+
+test("Should get profile for user", async () => {
+  await request(app)
+    .get("/users/me")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+});
+
+test("Shoul not get unauthorized user", async () => {
+  await request(app).get("/users/me").send().expect(401);
+});
+
+test("Should delete user", async () => {
+  await request(app)
+    .delete("/users/me")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+});
+
+test("Should not delete if unauthorized", async () => {
+  await request(app).delete("/users/me").send().expect(401);
 });
